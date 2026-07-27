6b52eac5b831b80c0d4fd5bd50d448d46a31e63b
yogesh kannan <yogeshkannan.nk@gmail.com>
2026-07-27 11:48:16 +0530

    Fix logic for generating alternating bits

--- ex2/binAltNum.c
+++ ex2/binAltNum.c
@@ -1,13 +1,13 @@
 #include <stdbool.h>
 
 bool hasAlternatingBits(int n) {
-    long x = 1;                     // 1 = 1
-    while (x <= n) {                // 2 = 10
-        if (x == n)                 // 5 = 101
-            return true;            // 10 = 1010
-        else if (x % 2 == 0)        // 21 = 10101
-            x = 2 * x + 1;          // 42 = 101010
-        else                        // 85 = 1010101
+    long x = 1;                    
+    while (x <= n) {              
+        if (x == n)               
+            return true;            
+        else if (x % 2 == 0)       
+            x = 2 * x + 1;          
+        else                       
             x = 2 * x;
     }
     return false;
